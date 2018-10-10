832c73dcc49fa9617e55f5a53ba2921c3396b514
sophie <osipovaa123@yandex.ru>
2018-10-10 13:22:08 +0300

    Update class program

--- HelloWorldRSOI/Program.cs
+++ HelloWorldRSOI/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace HelloWorldRSOI
 {
@@ -14,18 +7,7 @@ namespace HelloWorldRSOI
     {
         public static void Main(string[] args)
         {
-          //  CreateWebHostBuilder(args).Build().Run();
-
-            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
-            var host = new WebHostBuilder()
-            .UseKestrel()
-            .UseContentRoot(Directory.GetCurrentDirectory())
-            .UseConfiguration(config)
-            .UseIISIntegration()
-            .UseStartup<Startup>()
-            .Build();
-
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();    
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
